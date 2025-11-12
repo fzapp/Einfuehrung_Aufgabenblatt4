d44283d7144f825f192259c595e1ecf2593cb36c
fzapp <fzapp@smail.uni-loeln.de>
2025-11-12 12:53:46 +0100

    aenderung_Einfuehrung_Aufgabenblatt4_Aufg2

--- src/p1/P1_main.java
+++ src/p1/P1_main.java
@@ -3,6 +3,49 @@ package p1;
 public class P1_main {
 
 	public static void main(String[] args) {
+		int stockwerk = 0;
+		
+		switch(stockwerk) {
+		case 0 :
+			System.out.println("Angebote");
+			break;
+		case 1 :
+			System.out.println("Damenbekleidung");
+			break;
+		case 2 :
+			System.out.println("Herrenbekleidung");
+			break;
+		case 3 :
+			System.out.println("Kinderbekleidung");
+			break;
+		case -1:
+			System.out.println("Parkhaus");
+			break;
+		default :
+			System.out.println("Unbekannt");
+			break;
+		}
+		
+		System.out.println(" Aufgabe 2");
+		
+		switch(stockwerk) {
+		case -1 :
+			System.out.println("Parkhaus");
+			break;
+		case 3 :
+			System.out.println("Kinderbekleidung");
+		case 2 :
+			System.out.println("Herrenbekleidung");
+		case 1 :
+			System.out.println("Damenbekleidung");
+		case 0 :
+			System.out.println("Kinderbekleidung");
+			break;
+		default :
+			System.out.println("unbekannt");
+			break;
+		}
+		
 		// TODO Auto-generated method stub
 
 	}
